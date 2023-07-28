8c0ad702edf66837ca2361e1d9802926689d09e2
Zashiky <119845760+ZashikyOff@users.noreply.github.com>
2023-07-28 20:29:10 +0200

    gitignore

--- core/Entity/config.php
+++ core/Entity/config.php
@@ -2,5 +2,5 @@
 // Connexion à la base de données
         $dsn = "mysql:host=localhost;port=3306;dbname=cinelist;charset=utf8";
         $dbUser = "root";
-        $dbPassword = "root";
+        $dbPassword = "";
         $lienDB = new PDO($dsn, $dbUser, $dbPassword);
